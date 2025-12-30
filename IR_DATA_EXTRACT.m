b82ec06185e90ce0b4210ac5556185f26f9fd31b
Limbotime2 <azielllimbo@gmail.com>
2025-12-30 15:36:46 -0500

    modified IR_DATA_EXTRACT to use multiple files instead of one

--- IR_DATA_EXTRACT.m
+++ IR_DATA_EXTRACT.m
@@ -1,46 +1,57 @@
-%initialize storage
-wavenumber=[];
-intensity=[];
+[File]=uigetfile('*.out','Select ORCA Output Files','MultiSelect','on');
 
+for ii=1:length(File)
 
-%open file
-fid=fopen('TFTFE_C3.out'); %REPLACE WITH NAME OF INPUT FILE
+    filename=char(File(ii));
 
-%what to look for
-pattern='IR SPECTRUM';
+    fid = fopen(filename,'r');
 
-%search for it and extract
-while ~feof(fid)
-    line=fgetl(fid);
-    if contains(line,pattern)
-        for i=1:5
-            line=fgetl(fid);
-        end
-        data_count=0;
-        while ~feof(fid)
-            line=fgetl(fid);
-            if isempty(line)
-                break;
+    %initialize storage
+    wavenumber=[];
+    intensity=[];
+
+    %what to look for
+    pattern='IR SPECTRUM';
+
+    %search for it and extract
+    while ~feof(fid)
+        line=fgetl(fid);
+        if contains(line,pattern)
+            for i=1:5
+                line=fgetl(fid);
             end
-            line_clean=strrep(line, ':',' ');
-            nums=sscanf(line_clean,'%f');
-            if length(nums)>4
-                wavenumber(end+1)=nums(2);
-                intensity(end+1)=nums(4);
+            data_count=0;
+            while ~feof(fid)
+                line=fgetl(fid);
+                if isempty(line)
+                    break;
+                end
+                line_clean=strrep(line, ':',' ');
+                nums=sscanf(line_clean,'%f');
+                if length(nums)>4
+                    wavenumber(end+1)=nums(2);
+                    intensity(end+1)=nums(4);
+                end
             end
+            break;
         end
-        break;
     end
+
+    %close the input file for good programming practice <3
+    fclose(fid);
+
+    %write data to an output file
+    [~,baseName,~]=fileparts(filename);
+    outputfilename=sprintf('%s_IRDATA.dat',baseName);
+    fid_out=fopen(outputfilename,'w');
+    for i=1:length(wavenumber)
+        %convert intensity values to cm/molecule
+        intensity(i)=intensity(i)*(100000/(6.02214129e23));
+        fprintf(fid_out,'%.4f\t%.6e\n',wavenumber(i),intensity(i));
+    end
+
+    %close the output file for good programming practice <3
+    fclose(fid_out);
+
+    fprintf('Successfully created for %s\n',outputfilename)
 end
-
-%close the input file for good programming practice <3
-fclose(fid);
-
-%write data to an output file
-fid_out=fopen('TFTFE_C3_IRDATA.dat','w'); %REPLACE WITH NAME OF OUTPUT
-for i=1:length(wavenumber)
-    fprintf(fid_out,'%.4f\t%.6f\n',wavenumber(i),intensity(i));
-end
-
-%close the output file for good programming practice <3
-fclose(fid_out);
